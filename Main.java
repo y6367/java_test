c45d2f62e10b5473fe3fdf923fad786d4864400e
joeyu <joewah.yu@seattlecolleges.edu>
2025-08-03 16:32:47 -0700

    Made file

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
+
+    String name;
+
+    public Main(String rapName) {
+        name = rapName;
+    }
+
     public static void main(String[] args) {
+        Main youngThug = new Main("Young Thug");
+        System.out.println(youngThug.name);
 
     }
 }
