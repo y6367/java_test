c355fb616ebfc5f2142d296e7bb5621a9110248e
joew <joeyu88775@gmail.com>
2025-08-03 17:17:15 -0700

    Made file

--- Main.java
+++ Main.java
@@ -21,6 +21,7 @@ public class Main {
         System.out.println(youngThug.hello());
         numberAddition();
         System.out.println(num);
-
+        numberAddition();
+        System.out.println(num);
     }
 }
