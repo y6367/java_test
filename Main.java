2cba24fbf804f8d807132f4d94ba37e5090e0dac
joeyu <joewah.yu@seattlecolleges.edu>
2025-08-03 17:02:46 -0700

    Made file

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     String name;
+    static int num;
 
     public Main(String rapName) {
         name = rapName;
@@ -10,9 +11,19 @@ public class Main {
         return "Hello " + name;
     }
 
+    public static int numberAddition() {
+        return num += 1;
+    }
+
     public static void main(String[] args) {
         Main youngThug = new Main("Young Thug");
         System.out.println(youngThug.name);
         System.out.println(youngThug.hello());
+        numberAddition();
+        System.out.println(num);
+        numberAddition();
+        System.out.println(num);
+        numberAddition();
+        System.out.println(num);
     }
 }
