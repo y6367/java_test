583fe91b9465fb7d7886e9e1ad6d241740572dfa
joew <joeyu88775@gmail.com>
2025-08-10 14:59:03 -0700

    Created Inheritance for employee hierarchy

--- Inheritance/Management.java
+++ Inheritance/Management.java
@@ -6,6 +6,7 @@ public class Management {
 
         PartTimeEmployee pte1 = new PartTimeEmployee("Jennifer", 1,40000,true, 1000);
 
+        System.out.println(fte1.calculateSalary());
         System.out.println(fte1.printSalary());
         System.out.println(pte1.printSalary());
     }
