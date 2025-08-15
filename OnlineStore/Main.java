7905048c17d5c2bfa29eda7eeb66e4e653a11b5c
joewah <joeyu88775@gmail.com>
2025-08-15 08:42:46 -0700

    Added view cart and checkout option

--- OnlineStore/Main.java
+++ OnlineStore/Main.java
@@ -16,68 +16,108 @@ public class Main {
     static Clothing c3 = new Clothing("French Trench Coat", 350);
 
 
-    Electronics e1 = new Electronics("Iphone", 700);
-    Electronics e2 = new Electronics("AirPods", 150);
-    Electronics e3 = new Electronics("Macbook", 1000);
+    static Electronics e1 = new Electronics("Iphone", 700);
+    static Electronics e2 = new Electronics("AirPods", 150);
+    static Electronics e3 = new Electronics("Macbook", 1000);
 
-    Food f1 = new Food("Bread", 10);
-    Food f2 = new Food("Steak", 40);
-    Food f3 = new Food("Lobster", 40);
+    static Food f1 = new Food("Bread", 10);
+    static Food f2 = new Food("Steak", 40);
+    static Food f3 = new Food("Lobster", 40);
 
     static String[] clothingName = {c1.name, c2.name, c3.name};
-    String[] foodName = {f1.name, f2.name, f3.name};
-    String[] electronicsName = {e1.name, e2.name, e3.name};
+    static String[] foodName = {f1.name, f2.name, f3.name};
+    static String[] electronicsName = {e1.name, e2.name, e3.name};
     static int[] clothingPrice = {c1.price, c2.price, c3.price};
-    int[] foodPrice = {f1.price, f2.price, f3.price};
-    int[] electronicsPrice = {c1.price, c2.price, c3.price};
+    static int[] foodPrice = {f1.price, f2.price, f3.price};
+    static int[] electronicsPrice = {c1.price, c2.price, c3.price};
+
+    public static void getConfirmation(String[] categoryName, int[] categoryPrice, int input) {
+        System.out.println("Please confirm " + categoryName[input-1] + "for $" + categoryPrice[input-1] + ". Type 1 or 2");
+        int confirm = scanner.nextInt();
+        if (confirm == 1) {
+            items.add(categoryName[input-1]);
+            price.add(categoryPrice[input-1]);
+            System.out.println(categoryName[input-1] + " has been added to cart!");
+        }
+        else {
+            System.out.println("Item not added to cart");
+        }
+    }
 
     public static void getPurchase() {
-        System.out.println("====================\nMain Menu\n=====================");
-        System.out.println("1. Clothing");
-        System.out.println("2. Electronics");
-        System.out.println("3. Food");
-        System.out.println("Enter your choice by number (1, 2, 3)");
-        int input = scanner.nextInt();
+        boolean running = true;
+        while (running) {
+            System.out.println("====================\nMain Menu\n=====================");
+            System.out.println("1. Clothing");
+            System.out.println("2. Electronics");
+            System.out.println("3. Food");
+            System.out.println("4. View cart");
+            System.out.println("5. Checkout");
+            System.out.println("Enter your choice by number (1, 2, 3, 4, 5)");
+            int input = scanner.nextInt();
 
-        switch (input) {
-            case 1:
-                System.out.println("====================\nClothing\n=====================");
-                System.out.println("1. Rick & Morty T-shirt");
-                System.out.println("2. ATL Jeans");
-                System.out.println("3. French Trench Coat");
-                System.out.println("Enter your choice by number (1, 2, 3)");
-                input = scanner.nextInt();
+            switch (input) {
+                case 1:
+                    System.out.println("Clothing");
+                    System.out.println("1. Rick & Morty T-shirt");
+                    System.out.println("2. ATL Jeans");
+                    System.out.println("3. French Trench Coat");
+                    System.out.println("Enter your choice by number (1, 2, 3)");
+                    input = scanner.nextInt();
 
-                System.out.println("Please confirm " + clothingName[input-1] + "for $" + clothingPrice[input-1] + ". Type yes or no");
-                String confirm = scanner.next();
-                if (confirm.equals("yes")) {
-                    items.add(clothingName[input-1]);
-                    price.add(clothingPrice[input-1]);
-                    System.out.println(clothingName[input-1] + " has been added to cart!");
+                    getConfirmation(clothingName, clothingPrice, input);
                     break;
-                }
-                else {
-                    System.out.println("Item not added to cart");
+
+                case 2:
+                    System.out.println("Electronics");
+                    System.out.println("1. Iphone");
+                    System.out.println("2. AirPods");
+                    System.out.println("3. Macbook");
+                    System.out.println("Enter your choice by number (1, 2, 3)");
+                    input = scanner.nextInt();
+
+                    getConfirmation(electronicsName, electronicsPrice, input);
                     break;
-                }
 
-            case 2:
-                System.out.println("====================\nElectronics\n=====================");
-                System.out.println("1. Iphone");
-                System.out.println("2. AirPods");
-                System.out.println("3. Macbook");
-                System.out.println("Enter your choice by number (1, 2, 3)");
-                input = scanner.nextInt();
+                case 3:
+                    System.out.println("Food");
+                    System.out.println("1. Bread");
+                    System.out.println("2. Steak");
+                    System.out.println("3. Lobster");
+                    System.out.println("Enter your choice by number (1, 2, 3)");
+                    input = scanner.nextInt();
 
-            case 3:
-                System.out.println("====================\nFood\n=====================");
-                System.out.println("1. Bread");
-                System.out.println("2. Steak");
-                System.out.println("3. Lobster");
-                System.out.println("Enter your choice by number (1, 2, 3)");
-                input = scanner.nextInt();
-                System.out.println();
+                    getConfirmation(foodName, foodPrice, input);
+                    break;
 
+                case 4:
+                    System.out.println("Cart");
+                    for (int i = 0; i < items.size(); i++) {
+                        System.out.print((i + 1) + ". " + items.get(i));
+                        System.out.println(" for $" + price.get(i));
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Checkout");
+                    int total = 0;
+                    for (int i : price) {
+                        total += i;
+                    }
+                    System.out.println("You owe $" + total);
+
+                    System.out.println("Please confirm to pay $" + total + " type 1 or 2");
+                    int confirm = scanner.nextInt();
+                    if (confirm == 1) {
+                        System.out.println("You paid $" + total + ", have a good day!");
+                        running = false;
+                        break;
+                    }
+                    else {
+                        System.out.println("Cart has not been paid");
+                        break;
+                    }
+            }
         }
     }
 
